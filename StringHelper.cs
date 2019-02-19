034b3fdd42b71c43cec1ceaf48ee55b737dfef3f
Nikita Tret <accauntnikity@gmail.com>
2019-02-19 17:03:40 +0300

    implemented

--- StringHelper.cs
+++ StringHelper.cs
@@ -4,10 +4,33 @@ namespace PadawansTask4
 {
     public class StringHelper
     {
+        public static void Main(string[] args)
+        {
+            int vowels = GetVowelCount("kkaaakr");
+            Console.WriteLine(vowels);
+            Console.Read();
+        }
+        //static void Main(string[] args)
+        //{
+        //    int vowels = GetVowelCount("kkaaakr");
+        //    Console.WriteLine(vowels);
+        //    Console.Read();
+        //}
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            int count = 0;
+            //str = Console.ReadLine();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a' || str[i] == 'i' || str[i] == 'u' || str[i] == 'o' || str[i] == 'e')
+                {
+                    count++;
+                }
+
+            }
+            return count;
+
+
         }
     }
 }
